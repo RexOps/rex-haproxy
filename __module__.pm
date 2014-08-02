9f210dcb0ac490c706d0fa46755152e2676d5328
Jan <krimdomu@googlemail.com>
2014-08-02 16:49:16 +0200

    add firewall and routing rules if send_client_ip is used

--- __module__.pm
+++ __module__.pm
@@ -35,6 +35,40 @@ task "setup", make {
     on_change => make {
     service haproxy => "restart";
     };
+
+  # check if somewhere in the backend configurations
+  # send_client_ip is set to true (1)
+  # if this is the case we need to set some iptables,
+  # routing and sysctl rules.
+
+  # these rules can also be found on:
+  # http://blog.loadbalancer.org/configure-haproxy-with-tproxy-kernel-for-full-transparent-proxy/
+
+  my ($is_send_client_ip) = grep { $_->{send_client_ip} } @{$backends};
+  if ($is_send_client_ip) {
+    sysctl "net.ipv4.conf.all.forwarding"     => 1;
+    sysctl "net.ipv4.conf.all.send_redirects" => 1;
+
+    run "iptables -t mangle -N DIVERT",
+      unless => "iptables-save | grep ':DIVERT'";
+
+    run "iptables -t mangle -A PREROUTING -p tcp -m socket -j DIVERT",
+      unless =>
+      "iptables-save  | grep -- '-A PREROUTING -p tcp -m socket -j DIVERT'";
+
+    run "iptables -t mangle -A DIVERT -j MARK --set-mark 777",
+      unless =>
+      "iptables-save  | grep -- '-A DIVERT -j MARK --set-xmark 0x309'";
+
+    run "iptables -t mangle -A DIVERT -j ACCEPT",
+      unless => "iptables-save | grep -- '-A DIVERT -j ACCEPT'";
+
+    run "ip rule add fwmark 777 lookup 700",
+      unless => "ip rule list | grep 0x309";
+
+    run "ip route add local 0.0.0.0/0 dev lo table 700",
+      unless => "ip route list table 700 | grep 'local default dev lo'";
+  }
 };
 
 1;
